87510c55601defe53d50d84a42577f98d12ae772
Nombre prueba <prueba@ejemplo.com>
2024-09-12 13:20:08 +0200

    Añadiendo carpeta módulo 4

---
+++ Modulo4/src/main.ts
@@ -0,0 +1,71 @@
+let turno = 0
+
+function pasarSiguienteTurno() {
+  turno = turno + 1;
+  const elementoDiv = document.getElementById("turno");
+  if (elementoDiv !== null && elementoDiv !== undefined){
+    elementoDiv.innerText = `${turno}`.padStart(2, "0");
+  };
+};
+
+//mostrar el resultado 
+const botonSiguiente = document.getElementById("botonSiguiente");
+if(botonSiguiente !== null && botonSiguiente !== undefined && botonSiguiente instanceof HTMLButtonElement) {
+    botonSiguiente.addEventListener("click" ,() => {
+        pasarSiguienteTurno();
+    });
+};
+
+function volverTurnoAnterior() {
+    if (turno > 0){
+    turno = turno -1 ;
+    const elementoDiv = document.getElementById("turno");
+    if (elementoDiv !== null && elementoDiv !== undefined){
+        elementoDiv.innerText = `${turno}`.padStart(2, "0");
+    };
+    }
+    
+  };
+  
+  //mostrar el resultado 
+  const botonAnterior = document.getElementById("botonAnterior");
+  if(botonAnterior !== null && botonAnterior !== undefined && botonAnterior instanceof HTMLButtonElement) {
+      botonAnterior.addEventListener("click" ,() => {
+          volverTurnoAnterior();
+      });
+  };
+
+  function resetear() {
+    turno = 0;
+    const elementoDiv = document.getElementById("turno");
+    if (elementoDiv !== null && elementoDiv !== undefined){
+      elementoDiv.innerText = `${turno}`.padStart(2, "0");
+    };
+  };
+
+  //mostrar el resultado 
+  const botonReset = document.getElementById("botonReset");
+  if(botonReset !== null && botonReset !== undefined && botonReset instanceof HTMLButtonElement) {
+      botonReset.addEventListener("click" ,() => {
+          resetear();
+      });
+  };
+
+function enviarNumero () {
+    const elementoInput = document.getElementById("numeroManual");
+    if(elementoInput !== null && elementoInput !== undefined && elementoInput instanceof HTMLInputElement) {
+        turno = parseInt(elementoInput.value);
+        const elementoDiv = document.getElementById("turno");
+        if (elementoDiv !== null && elementoDiv !== undefined){
+          elementoDiv.innerText = `${turno}`.padStart(2, "0");
+        };
+    }
+};
+const botonEnviar = document.getElementById("botonEnviar");
+ if(botonEnviar !== null && botonEnviar !== undefined && botonEnviar instanceof HTMLButtonElement){
+    botonEnviar.addEventListener("click", () => { 
+        enviarNumero();
+    })
+ }
+
+
